19a5b8908bac463e15beace33285532b09e514f9
Mateusz Gostański <mg@grixu.dev>
2021-01-04 11:52:12 +0100

    Refactored config

--- config/config.php
+++ config/config.php
@@ -4,10 +4,6 @@
  * You can place your custom package configuration in here.
  */
 return [
-    'base_url' => env('API_BASE_URL'),
-    'client_key' => env('API_CLIENT_KEY'),
-    'client_id' => env('API_CLIENT_ID'),
-    'oauth' => env('API_OAUTH'),
-    'logging' => env('API_ERROR_LOGGING', true),
+    'logging' => env('API_ERROR_LOGGING', false),
     'log_channel' => env('API_ERROR_LOG_CHANNEL', 'api-client'),
 ];
