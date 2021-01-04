56684a04c7d5585d7e08bff02e05523786d7a32e
Mateusz Gostański <mg@grixu.dev>
2021-01-04 11:50:49 +0100

    Updated PHPDoc in ApiClientFacade

--- src/ApiClientFacade.php
+++ src/ApiClientFacade.php
@@ -4,6 +4,9 @@ namespace Grixu\ApiClient;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static make(mixed $env)
+ */
 class ApiClientFacade extends Facade
 {
     protected static function getFacadeAccessor()
