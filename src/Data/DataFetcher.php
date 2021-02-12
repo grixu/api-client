bf943d0753167b152cad965e9e4c9a75375d2920
Mateusz Gostański <mg@grixu.dev>
2021-02-12 09:37:41 +0100

    Created DataFetcher class
    It responsibility is to call API by HTTP using given URI, pass Response object (Guzzle|PSR-7) to given responseClass.

---
+++ src/Data/DataFetcher.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Grixu\ApiClient\Data;
+
+use Grixu\ApiClient\Contracts\FetchedData;
+use Grixu\ApiClient\Contracts\TokenAuth;
+use Grixu\ApiClient\Exceptions\AccessDeniedException;
+use Grixu\ApiClient\Exceptions\ApiCallException;
+use GuzzleHttp\Psr7\Uri;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+
+class DataFetcher
+{
+    protected FetchedData $fetchedData;
+
+    public function __construct(protected Uri $uri, protected string $responseClass, protected ?TokenAuth $token = null)
+    {
+    }
+
+    public function get(): FetchedData
+    {
+        if (empty($this->fetchedData)) {
+            $this->fetch();
+        }
+
+        return $this->fetchedData;
+    }
+
+    public function fetch(): void
+    {
+        $response = $this->makeRequest();
+
+        $this->handleHttpErrors($response);
+
+        if ($response->successful()) {
+            $this->fetchedData = new $this->responseClass($response);
+        }
+    }
+
+    protected function makeRequest(): Response
+    {
+        $request = Http::withHeaders(
+            [
+                'Accept' => 'application/json'
+            ]
+        );
+
+        if ($this->token) {
+            $request = $request->withToken($this->token->getToken());
+        }
+
+        return $request->get($this->uri);
+    }
+
+    protected function handleHttpErrors(Response $response)
+    {
+        if ($response->failed()) {
+            match ($response->status()) {
+                401 => $this->handleUnauthorized(),
+                403 => throw new AccessDeniedException($response->body()),
+                default => throw new ApiCallException($this->uri, $response->body())
+            };
+        }
+    }
+
+    protected function handleUnauthorized(): void
+    {
+        $this->token->reset();
+        $this->fetch();
+    }
+}
