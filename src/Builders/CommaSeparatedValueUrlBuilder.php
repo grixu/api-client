433c6fd97aee7cb2836b4d985935671666620643
Mateusz Gostański <mg@grixu.dev>
2021-02-12 08:27:04 +0100

    Created CommaSeparatedValueUrlBuilder
    This class realize building parts as a: ?include=some,relationship,etc

---
+++ src/Builders/CommaSeparatedValueUrlBuilder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Grixu\ApiClient\Builders;
+
+use Illuminate\Support\Collection;
+
+class CommaSeparatedValueUrlBuilder extends AbstractMultiParamUrlBuilder
+{
+    private string $param;
+    private Collection $values;
+
+    public function get(): string
+    {
+        if (empty($this->param) || empty($this->values))
+            return '';
+
+        return $this->param.'='.$this->values->join(',');
+    }
+
+    public function add($param, ...$values): static
+    {
+        $this->checkInConfig($values);
+
+        $this->param = $param;
+        $this->values = collect($values);
+
+        return $this;
+    }
+}
