8b31b8cfd8250503d96ba586b63ff65738299830
Mateusz Gostański <mg@grixu.dev>
2021-02-12 09:27:22 +0100

    Created TokenAuth interface

---
+++ src/Contracts/TokenAuth.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Grixu\ApiClient\Contracts;
+
+
+interface TokenAuth
+{
+    public function getToken(): string;
+    public function authorize(): void;
+    public function reset(): void;
+}
